cc3c721d59f700e76d820563f372c77fcd59a4e6
Lauren Trotter <lauren94trotter@gmail.com>
2026-02-26 12:53:37 +0000

    Initial repo setup with console and test projects

---
+++ LibraryApp/Program.cs
@@ -0,0 +1,9 @@
+﻿namespace LibraryApp;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}
---
+++ LibraryAppTests/UnitTest1.cs
@@ -0,0 +1,10 @@
+﻿namespace LibraryAppTests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
